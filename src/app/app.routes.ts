edf9a9007684357091f2b94061f9a482fdc89c79
Amol Bodhare <amolfeb95@gmail.com>
2020-12-29 02:48:24 +0530

    Add login route as base route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from '@src/app/core/components/login/login.component';
 export const routes: Routes = [
   {
       path: '',
-      redirectTo: '/home',
+      redirectTo: '/login',
       pathMatch: 'full',
   },
   {
