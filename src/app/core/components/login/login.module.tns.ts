3a2471d24a3f093466155de17b62c3ae11510db2
Amol Bodhare <amolfeb95@gmail.com>
2020-12-29 23:31:52 +0530

    Add web html for login page

--- src/app/core/components/login/login.module.tns.ts
+++ src/app/core/components/login/login.module.tns.ts
@@ -6,8 +6,7 @@ import { LoginComponent } from './login.component';
   declarations: [
     LoginComponent
   ],
-  imports: [
-    
-  ]
+  imports: [],
+  exports: [LoginComponent]
 })
 export class LoginModule { }
