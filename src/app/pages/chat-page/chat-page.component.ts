99e8b46af96dcf345e4a91519a9b02ced3ad7615
Amol Bodhare <amolfeb95@gmail.com>
2021-01-11 03:49:15 +0530

    Mobile UI for chat page

--- src/app/pages/chat-page/chat-page.component.ts
+++ src/app/pages/chat-page/chat-page.component.ts
@@ -6,7 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-page.component.css']
 })
 export class ChatPageComponent implements OnInit {
-
+  messages = [
+    {
+      message: "Hi, this is Amol",
+      from: true
+    },
+    {
+      message: "Hi, this is Amol",
+      from: true
+    },
+    {
+      message: "Hi, this is Amol",
+      from: true
+    },
+    {
+      message: "Hi, this is Amol",
+      from: false
+    },
+    {
+      message: "Hi, this is Amol",
+      from: true
+    },
+    {
+      message: "Hi, this is Amol",
+      from: true
+    },
+    {
+      message: "Hi, this is Amol",
+      from: false
+    },
+    {
+      message: "Hi, this is Amol",
+      from: true
+    },
+    {
+      message: "Hi, this is Amol and it is a big sentence",
+      from: false
+    },
+    {
+      message: "Hi, this is Amol22222, Hi, this is Amol22222, Hi, this is Amol22222, Hi, this is Amol22222",
+      from: true
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
